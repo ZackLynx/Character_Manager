743cf545c595d8c8de60045257e0972f13cc0d35
ZackLynx <60022299+ZackLynx@users.noreply.github.com>
2025-03-31 20:14:55 -0500

    Initial work on character sheet form refactor

---
+++ js/skills_audocalc.js
@@ -0,0 +1,18 @@
+/*
+-----------------------------------------------------------------------------------------------
+Name:		skills_autocalc.js
+Author:		Connor Bryan Andrew Clawson
+Date:		2025-03-30
+Language:	JavaScript
+Purpose:	This script handles the automatic calculation for skill bonus totals
+
+-----------------------------------------------------------------------------------------------
+ChangeLog:
+Who			When			What
+----------- --------------- -------------------------------------------------------------------
+CBAC		2025-03-30		Original Version 
+-----------------------------------------------------------------------------------------------
+*/
+console.log("Hello, World!");
+
+//execute whenever a value changes.
