d691abaa5740367e2256cd7b4893257474a4faad
ZackLynx <60022299+ZackLynx@users.noreply.github.com>
2025-04-26 04:47:28 -0500

    Just need documentation and function docstrings.

--- sql/inventory.sql
+++ sql/inventory.sql
@@ -2,7 +2,7 @@
 -----------------------------------------------------------------------------------------------
 Name:       inventory.sql
 Author:     Connor Bryan Andrew Clawson
-Date:       YYYY-MM-DD
+Date:       2025-04-23
 Language:   SQL
 System:     MySQL 8.2.12
 Purpose:    This query creates the inventory table for tracking character inventories.
@@ -11,14 +11,19 @@ Purpose:    This query creates the inventory table for tracking character invent
 ChangeLog:
 Who         When            What
 ----------- --------------- -------------------------------------------------------------------
-CBAC        YYYY-MM-DD      Original Version
+CBAC        2025-04-23      Original Version
+CBAC        2025-04-26      Table columns and Foreign key Constraints finalized.
 -----------------------------------------------------------------------------------------------
 */
 
 DROP TABLE IF EXISTS `inventory`;
+
 CREATE TABLE `inventory` (
     Inventory_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     Character_ID INT NOT NULL,
     Item_Name VARCHAR(255) NOT NULL,
     Item_Desc TEXT
 );
+
+ALTER TABLE `inventory` ADD CONSTRAINT `FK_Inv_Character` 
+FOREIGN KEY (`Character_ID`) REFERENCES `characters` (`Character_ID`);
